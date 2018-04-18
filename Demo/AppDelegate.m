81071cf705a72f8aa31accd15e1a8028d1eeb7d2
tyrionnie <tyrionnie@upchina.com>
2018-04-18 17:31:44 +0800

    initial commit

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Test1ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    Test1ViewController *test1ViewController = [[Test1ViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:test1ViewController];
+    self.window.rootViewController = navigationController;
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
---
+++ Demo/Test1ViewController.m
@@ -0,0 +1,31 @@
+//
+// Created by Nic on 2018/4/18.
+// Copyright (c) 2018 UpChina. All rights reserved.
+//
+
+#import "Test1ViewController.h"
+
+@interface Test1ViewController ()
+
+
+@end
+
+@implementation Test1ViewController
+
+- (instancetype)initWithNibName:(nullable NSString *)nibNameOrNil bundle:(nullable NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"test1";
+
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+
+@end
